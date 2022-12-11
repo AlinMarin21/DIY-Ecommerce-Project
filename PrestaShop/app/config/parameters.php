39f8266bd9ecc37f5523c9bff4807edb3b3ceee4
Alin Marin <alin.ma21@yahoo.com>
2022-12-11 22:22:31 +0200

    PrestaShop folder part1

---
+++ PrestaShop/app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'bricolaj-db',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '15GeIVis57MGRigkjpQNFEBftF393aSNd3vx5gEQ8d2DJT5gy9A5hIreEw3AGEky',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2022-11-11',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'WdwYYGWJciJqpFurKPwLiHEXaoHLyuSXaIHiM4hvJM1mrKZSaxeKOvsQY3ty2AA4',
+    'cookie_iv' => '73b0Oy6i7KQExIJ0PtfYPm4CYFUomqhF',
+    'new_cookie_key' => 'def0000099ce58e2fd9f6b368410968b9559f483b493331b8fdc53935f0506c8de04a58e672bbe3cc29e105d7031c81010bae2ef14f6a040daaa7b3d2af26c00689f5262',
+  ),
+);
